47c6a4dc103ce5d2d4447732bff6224510cab6ab
Ganesh Setty Raghavan <setty.ganesh@gmail.com>
2017-03-05 18:21:30 +0530

    updated train SVC function file

--- train.py
+++ train.py
@@ -20,14 +20,14 @@ def features_extract(imagename_list):
     notcars = imagename_list[1]
 
     t=time.time()
-    n_samples = 2000
-    random_idxs = np.random.randint(0, len(cars), n_samples)
+    #n_samples = 2000
+    #random_idxs = np.random.randint(0, len(cars), n_samples)
 
-    test_cars = np.array(cars)[random_idxs]
-    test_notcars = np.array(notcars)[random_idxs]
+    #test_cars = np.array(cars)[random_idxs]
+    #test_notcars = np.array(notcars)[random_idxs]
     
-    #test_cars = np.array(cars)
-    #test_notcars = np.array(notcars)
+    test_cars = np.array(cars)
+    test_notcars = np.array(notcars)
 
     ### TODO: Tweak these parameters and see how the results change.
     color_space = 'YCrCb' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
@@ -76,22 +76,29 @@ def features_extract(imagename_list):
 
 def train(car_features,notcar_features,parameters):
     
+    # Verical stacking of car features and notcar features
     X = np.vstack((car_features,notcar_features)).astype(np.float64)
 
+    # Compute the mean and std of stacked features to be used for later scaling.
     X_scaler = StandardScaler().fit(X)
 
+    # Use mean and std computed to perform standardization by centering and scaling
     scaled_X = X_scaler.transform(X)
 
+    # Labeling '1' for cars features and '0' for notcar features
     y = np.hstack((np.ones(len(car_features)),np.zeros(len(notcar_features))))
 
+    # randomize a selection for training(80%) and testing(20%)
     rand_state = np.random.randint(0,100)
-
     X_train,X_test,y_train,y_test = train_test_split(scaled_X,y,test_size =0.20,random_state = rand_state)
 
+    # Linear Support Vector Classifier 
     svc = LinearSVC()
 
     t = time.time()
 
+    # fit to the training data , returning a "best fit" hyperplane that divides, or categorizes, 
+    # the  data and the learning is done.
     svc.fit(X_train,y_train)
 
     print(round(time.time() -t, 2),'seconds to train SVC')
